119ebf034bffcae682cc029aa7d59d4d08ae74a7
Drosscend <597-kveronesi@users.noreply.gitlab.iut-blagnac.fr>
2021-10-22 10:48:14 +0200

    update

--- Face_Capture_With_Rotate.py
+++ Face_Capture_With_Rotate.py
@@ -23,8 +23,7 @@ while Count < 50:
     if np.average(gray) > 110:  # Testing the brightness of the image
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # Detect the faces and store the positions
         for (x, y, w, h) in faces:  # Frames  LOCATION X, Y  WIDTH, HEIGHT
-            FaceImage = gray[y - int(h / 2): y + int(h * 1.5),
-                        x - int(x / 2): x + int(w * 1.5)]  # The Face is isolated and cropped
+            FaceImage = gray[y - int(h / 2): y + int(h * 1.5), x - int(x / 2): x + int(w * 1.5)]  # The Face is isolated and cropped
             Img = (NameFind.DetectEyes(FaceImage))
             cv2.putText(gray, "FACE DETECTED", (x + int((w / 2)), y - 5), cv2.FONT_HERSHEY_DUPLEX, .4, WHITE)
 
@@ -36,11 +35,12 @@ while Count < 50:
             cv2.waitKey(300)
             cv2.imshow("CAPTURED PHOTO", frame)  # show the captured image
             Count = Count + 1
-    cv2.putText(gray, "Photo restante a etre prise :" + str(Count) + "/50", (10, 10), cv2.FONT_HERSHEY_DUPLEX, .4, WHITE)
+    cv2.putText(gray, "Photo restante a etre prise :" + str(Count) + "/50", (10, 10), cv2.FONT_HERSHEY_DUPLEX, .4,
+                WHITE)
     cv2.putText(gray, "Appuyer sur \"q\" pour quitter le programme", (10, 30), cv2.FONT_HERSHEY_DUPLEX, .4, WHITE)
-    cv2.imshow('Face Recognition System Capture Faces', gray)  # Show the video
+    cv2.imshow('Système de reconnaissance des visages Capture des visages', gray)  # Show the video
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-print('LA CAPTURE DU VISAGE DU SUJET EST TERMINÉE')
+print('La capture du visage du sujet est terminé')
 cap.release()
 cv2.destroyAllWindows()
